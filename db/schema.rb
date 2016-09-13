e5c36367ca60a3aaa3d4b66b3284b9bf9c8d23f5
Jacques Mesnet <jacquesmesnet@gmail.com>
2016-09-12 19:24:20 -0500

    Nous passons à Mansonry

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160912222315) do
+ActiveRecord::Schema.define(version: 20160913001346) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.integer  "adress"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "pins", ["user_id"], name: "index_pins_on_user_id"
